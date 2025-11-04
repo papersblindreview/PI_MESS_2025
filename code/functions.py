2fd34f46ea6d0aedfcc89c9a1d16d2924fe2c44c
papersblindreview <lmenical@nd.edu>
2025-11-04 15:35:52 -0500

    Update functions.py

--- code/functions.py
+++ code/functions.py
@@ -1,1 +1,192 @@
 
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+os.environ["OMP_NUM_THREADS"] = "1"
+
+import pickle
+import numpy as np
+import tensorflow as tf
+
+
+def load_data():
+  time_vars, space_vars, depth, max_depth, temperature, year = [], [], [], [], [], []
+  
+  for f in [f for f in os.listdir('../data/train_val/') if 'part' in f]:
+    data = np.load('./data/train_val/' + f)
+    time_vars.append(data['time'])
+    space_vars.append( data['space'])
+    depth.append(data['depth'])
+    max_depth.append(data['max_depth'])
+    temperature.append(data['temperature'])
+    year.append(data['year'])
+
+  time_vars = np.vstack(time_vars)
+  space_vars = np.vstack(space_vars)
+  depth = np.vstack(depth)
+  max_depth = np.vstack(max_depth)
+  temperature = np.vstack(temperature)
+  year = np.concatenate(year)
+
+  return time_vars, space_vars, depth, max_depth, temperature, year
+
+def split_data(time_vars, space_vars, depth, max_depth, temperature, year, train_stop=2015):
+  time_train, space_train = time_vars[year <= train_stop,:], space_vars[year <= train_stop,:]
+  depth_train, max_depth_train = depth[year <= train_stop,:], max_depth[year <= train_stop,:]
+  temp_train = temperature[year <= train_stop,:]
+  
+  time_val, space_val = time_vars[year > train_stop,:], space_vars[year > train_stop,:]
+  depth_val, max_depth_val = depth[year > train_stop,:], max_depth[year > train_stop,:]
+  temp_val = temperature[year > train_stop,:]
+  
+  mins_time, maxs_time = time_train.min(axis=0), time_train.max(axis=0)
+  mins_space, maxs_space = space_train.min(axis=0), space_train.max(axis=0)
+  maxs_time[-1] = 2080
+  
+  time_train_scaled = (time_train-mins_time) / (maxs_time-mins_time)
+  time_val_scaled = (time_val-mins_time) / (maxs_time-mins_time)
+  
+  space_train_scaled = (space_train-mins_space) / (maxs_space-mins_space)
+  space_val_scaled = (space_val-mins_space) / (maxs_space-mins_space)
+  
+  datasets = {}
+  datasets['X_train'] = (time_train_scaled, space_train_scaled, depth_train, max_depth_train)
+  datasets['y_train'] = temp_train
+  datasets['X_val'] = (time_val_scaled, space_val_scaled, depth_val, max_depth_val)
+  datasets['y_val'] = temp_val
+  
+  scales = {}
+  scales['mins_time'] = mins_time
+  scales['maxs_time'] = maxs_time
+  scales['mins_space'] = mins_space
+  scales['maxs_space'] = maxs_space
+  
+  return datasets, scales
+  
+def load_full():
+  time_vars, space_vars, max_depth, dates, lonlat = [], [], [], [], []
+  sites = []
+  for f in [f for f in os.listdir('../data/test/') if 'npz' in f]:
+    data = np.load('./data/test/' + f)
+    time_vars.append(data['features_time'])
+    space_vars.append( data['features_space'])
+    max_depth.append(data['max_depth'])
+    dates.append(data['dates'])
+    lonlat.append(data['lonlat'])
+    
+  for i in [f for f in os.listdir('../data/test/') if 'info' in f]:
+    with open('./data/test/' + i, 'rb') as f:
+      sites_temp = pickle.load(f) 
+     
+    sites.append(sites_temp) 
+  
+  sites = np.concatenate(sites).reshape(-1,1)
+  time_vars = np.vstack(time_vars)
+  space_vars = np.vstack(space_vars)
+  max_depth = np.vstack(max_depth)
+  dates = np.vstack(dates)
+  lonlat = np.vstack(lonlat)
+
+  return time_vars, space_vars, max_depth, dates, lonlat, sites
+  
+
+@tf.keras.utils.register_keras_serializable() 
+class LakeTempLayerST(tf.keras.layers.Layer):
+  def __init__(self, **kwargs):
+    super(LakeTempLayerST, self).__init__(**kwargs)
+
+  def call(self, inputs):
+    raw_params, depth, max_depth = inputs
+    
+    T_bot = 1.0 + 45.*raw_params[:, 0:1] + 1e-3
+    T_top = 1e-3 + 45.*raw_params[:, 1:2]
+    a = 1e-3 + raw_params[:, 2:3] #tf.math.sigmoid() + 1e-3
+    k = 1e-2 + raw_params[:, 3:4] #tf.math.softplus() + 1e-3
+    
+    x = tf.clip_by_value((depth - a*max_depth) / k, -50.0, 50.0)
+    return T_bot + (T_top - T_bot) / (1 + tf.exp(x))
+    
+  @classmethod
+  def from_config(cls, config):
+    return cls(**config)
+  
+def build_st_model(time_shape, space_shape, time_sizes, space_sizes, comb_sizes,  drop_time, drop_space, drop_comb, suffix):
+    
+  input_time = tf.keras.layers.Input(shape=time_shape, name='Weather')
+  input_space = tf.keras.layers.Input(shape=space_shape, name='LakeChars') 
+  input_depth = tf.keras.layers.Input(shape=(1,), name='Depth')
+  input_max_depth = tf.keras.layers.Input(shape=(1,), name='MaxDepth')
+  x_time = input_time
+  x_space = input_space
+    
+  for i, s_t in enumerate(time_sizes):
+      x_time = tf.keras.layers.Dense(s_t, name=f'Dense_Weather{i+1}')(x_time)
+      x_time = tf.keras.layers.BatchNormalization(name=f'Norm_Weather{i+1}')(x_time)
+      x_time = tf.keras.layers.LeakyReLU(name=f'LeakyReLU_Weather{i+1}')(x_time)
+      x_time = tf.keras.layers.Dropout(drop_time[i], name=f'Drop_Weather{i+1}')(x_time)
+  
+  for i, s_s in enumerate(space_sizes):
+      x_space = tf.keras.layers.Dense(s_s, name=f'Dense_LakeChars{i+1}')(x_space)
+      x_space = tf.keras.layers.BatchNormalization(name=f'Norm_LakeChars{i+1}')(x_space)
+      x_space = tf.keras.layers.LeakyReLU(name=f'LeakyReLU_LakeChars{i+1}')(x_space)
+      x_space = tf.keras.layers.Dropout(drop_space[i], name=f'Drop_LakeChars{i+1}')(x_space)
+  
+  ###############################################################################################
+  if suffix == 'NPI' or suffix == 'MOE':
+    # Non-PINN branch
+    x_nonpinn = tf.keras.layers.Concatenate(name='Together_NonPINN')([x_time, x_space, input_depth, input_max_depth])
+    for i, s in enumerate(comb_sizes):
+        x_nonpinn = tf.keras.layers.Dense(s, name=f'Dense_Comb_NonPINN{i+1}')(x_nonpinn)
+        x_nonpinn = tf.keras.layers.BatchNormalization(name=f'Norm_Comb_NonPINN{i+1}')(x_nonpinn)
+        x_nonpinn = tf.keras.layers.LeakyReLU(name=f'LeakyReLU_Comb_NonPINN{i+1}')(x_nonpinn)
+        x_nonpinn = tf.keras.layers.Dropout(drop_comb[i], name=f'Drop_Comb_NonPINN{i+1}')(x_nonpinn)
+    x_nonpinn_last = tf.keras.layers.Dense(comb_sizes[-1], name='Dense_Last_NonPINN')(x_nonpinn)
+    temp_nonpi = tf.keras.layers.Dense(1, activation='sigmoid', name='ML_Temperature_sigmoid')(x_nonpinn_last)
+    temp_nonpi = tf.keras.layers.Lambda(lambda x: 1e-3 + 45.*x, name="ML_Temperature")(temp_nonpi)
+    ###############################################################################################
+    if suffix == 'NPI': 
+      return tf.keras.Model([input_time, input_space, input_depth, input_max_depth], temp_nonpi, name=f'ST_Model_{suffix}')
+     
+  # PINN branch
+  if suffix == 'MOE' or suffix == 'PINN':
+    x_pinn = tf.keras.layers.Concatenate(name='Together_PINN')([x_time, x_space])
+    for i, s in enumerate(comb_sizes):
+        x_pinn = tf.keras.layers.Dense(s, name=f'Dense_Comb_PINN{i+1}')(x_pinn)
+        x_pinn = tf.keras.layers.BatchNormalization(name=f'Norm_Comb_PINN{i+1}')(x_pinn)
+        x_pinn = tf.keras.layers.LeakyReLU(name=f'LeakyReLU_Comb_PINN{i+1}')(x_pinn)
+        x_pinn = tf.keras.layers.Dropout(drop_comb[i], name=f'Drop_Comb_PINN{i+1}')(x_pinn)
+    x_pinn_last = tf.keras.layers.Dense(comb_sizes[-1], name='Dense_Last_PINN')(x_pinn)
+    
+    T_bot = tf.keras.layers.Dense(1, activation='sigmoid', name='T_bot')(x_pinn_last) #sigmoid
+    T_top = tf.keras.layers.Dense(1, activation='sigmoid', name='T_top')(x_pinn_last) #sigmoid
+    a = tf.keras.layers.Dense(1, activation='sigmoid', name='a')(x_pinn_last)
+    k = tf.keras.layers.Dense(1, activation='relu', name='k')(x_pinn_last)
+    
+    params = tf.keras.layers.Concatenate(name='Parameters')([T_bot, T_top, a, k])
+    temp_pi = LakeTempLayerST(name='PINN_Temperature')([params, input_depth, input_max_depth])
+    ###############################################################################################
+    if suffix == 'PINN':
+      return tf.keras.Model([input_time, input_space, input_depth, input_max_depth], temp_pi, name=f'ST_Model_{moe}')
+  
+  weight_input = tf.keras.layers.Concatenate(name='Weight_Input')([x_time, x_space])
+  weight = tf.keras.layers.Dense(1, activation='sigmoid', name="Weight")(weight_input)
+  
+  # Combine outputs
+  temperature = tf.keras.layers.Lambda(
+      lambda inputs: inputs[2] * inputs[0] + (1.0 - inputs[2]) * inputs[1], name="Weighted_Temperature")([temp_pi, temp_nonpi, weight])
+  
+  return tf.keras.Model([input_time, input_space, input_depth, input_max_depth], temperature, name=f'ST_Model_{moe}')
+    
+
+class LossLogger(tf.keras.callbacks.Callback):
+  def __init__(self, filename):
+    super().__init__()
+    self.filename = filename
+    with open(self.filename, 'w') as f:
+      f.write("Model history\n")
+
+  def on_epoch_end(self, epoch, logs=None):
+    logs = logs or {}
+    with open(self.filename, 'a') as f:
+      f.write(f"{epoch+1}, Loss: {logs.get('loss'):.4f}, Val Loss: {logs.get('val_loss'):.4f}\n")  
+ 
+  
